3dcdc2d4d2b5890e21a2756a4445727fef8832ce
Adershow <adersonrs@gmail.com>
2019-04-13 18:30:17 -0300

    backend pronta

---
+++ src/server.js
@@ -0,0 +1,42 @@
+//Trazendo o express
+const express = require('express');
+
+//Trazendo o Cors
+const cors = require('cors');
+
+//Trazendo o mongo 
+const mongoose = require('mongoose');
+
+//Trazendo o path
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+//Trazendo o socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket =>{
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
+
+mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-qnbxm.mongodb.net/omnistack?retryWrites=true", {
+    useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require("./routes"));
+
+server.listen(3333);
